754295cd28c1fd34148538ca0dd528e20d714e8f
Jean Landim <jewanbb@gmail.com>
2020-06-24 12:36:17 -0300

    Create lucro_da_acao.py

---
+++ lucro_da_acao.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import Tuple, Union
+class LucroDaAcao:
+
+    def calcula_acao(self, acao:list) -> Union[Tuple, None]:  
+        """ Pega a lista de valores e verifica qual é o menor preço e depois 
+        qual é o maior preço. """
+        # Pega o valor minimo
+        valor_minimo = min(acao)
+        valor_maximo = max(acao)
+        
+        index_valor_minimo = acao.index(valor_minimo)
+        index_valor_maximo = acao.index(valor_maximo)
+
+        if index_valor_minimo < index_valor_maximo:
+           dia_da_acao = (index_valor_minimo+1, index_valor_maximo+1)  
+        else:
+           dia_da_acao = None 
+        
+        return dia_da_acao
+
+    def pega_acao(self, acao:list) -> Tuple: 
+        """ Retorna ao usuário o dia da compra e o dia da venda """
+        calcula_acao = self.calcula_acao(acao) 
+        if calcula_acao == None:
+           print("Não compensa comprar ação com este dados!")
+        else:
+           print(f"É vantajoso comprar ação no dia "+\
+                   f"{calcula_acao[0]} e vender no dia {calcula_acao[1]}")
+
+        return calcula_acao
+
+
+        
